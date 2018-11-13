a1beca725d3e435b67be4bd1dbdbf6e0db46b640
ustadenis <ustadenis@bk.ru>
2018-11-13 18:53:47 +0300

    Fix title on scaling

--- form/src/main/java/com/smarttable/data/format/title/ITitleDrawFormat.java
+++ form/src/main/java/com/smarttable/data/format/title/ITitleDrawFormat.java
@@ -3,8 +3,8 @@ package com.smarttable.data.format.title;
 import android.graphics.Canvas;
 import android.graphics.Rect;
 
-import com.smarttable.data.column.Column;
 import com.smarttable.core.TableConfig;
+import com.smarttable.data.column.Column;
 
 /**
  * Created by huang on 2017/11/6.
@@ -13,18 +13,19 @@ import com.smarttable.core.TableConfig;
 public interface ITitleDrawFormat {
 
     /**
-     *测量宽
+     * 测量宽
      */
     int measureWidth(Column column, TableConfig config);
 
     /**
-     *测量高
+     * 测量高
      */
     int measureHeight(TableConfig config);
 
     /**
      * 绘制
-     * @param c 画笔
+     *
+     * @param c      画笔
      * @param column 列信息
      */
     void draw(Canvas c, Column column, Rect rect, TableConfig config);
--- form/src/main/java/com/smarttable/data/format/title/TitleDrawFormat.java
+++ form/src/main/java/com/smarttable/data/format/title/TitleDrawFormat.java
@@ -4,8 +4,8 @@ import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Rect;
 
-import com.smarttable.data.column.Column;
 import com.smarttable.core.TableConfig;
+import com.smarttable.data.column.Column;
 import com.smarttable.data.format.bg.ICellBackgroundFormat;
 import com.smarttable.utils.DrawUtils;
 
@@ -29,35 +29,35 @@ public class TitleDrawFormat implements ITitleDrawFormat {
     public int measureHeight(TableConfig config) {
         Paint paint = config.getPaint();
         config.getColumnTitleStyle().fillPaint(paint);
-        return DrawUtils.getTextHeight(config.getColumnTitleStyle(),config.getPaint());
+        return DrawUtils.getTextHeight(config.getColumnTitleStyle(), config.getPaint());
     }
 
     @Override
     public void draw(Canvas c, Column column, Rect rect, TableConfig config) {
         Paint paint = config.getPaint();
-        boolean isDrawBg =drawBackground(c,column,rect,config);
+        boolean isDrawBg = drawBackground(c, column, rect, config);
         config.getColumnTitleStyle().fillPaint(paint);
         ICellBackgroundFormat<Column> backgroundFormat = config.getColumnCellBackgroundFormat();
 
-        paint.setTextSize(paint.getTextSize()*config.getZoom());
-        if(isDrawBg && backgroundFormat.getTextColor(column) != TableConfig.INVALID_COLOR){
+        paint.setTextSize(paint.getTextSize() * config.getZoom());
+        if (isDrawBg && backgroundFormat.getTextColor(column) != TableConfig.INVALID_COLOR) {
             paint.setColor(backgroundFormat.getTextColor(column));
         }
         drawText(c, column, rect, paint);
     }
 
     private void drawText(Canvas c, Column column, Rect rect, Paint paint) {
-        if(column.getTitleAlign() !=null) { //如果列设置Align ，则使用列的Align
+        if (column.getTitleAlign() != null) { //如果列设置Align ，则使用列的Align
             paint.setTextAlign(column.getTitleAlign());
         }
-        c.drawText(column.getColumnName(), DrawUtils.getTextCenterX(rect.left,rect.right,paint), DrawUtils.getTextCenterY((rect.bottom+rect.top)/2,paint) ,paint);
+        c.drawText(column.getColumnName(), DrawUtils.getTextCenterX(rect.left, rect.right, paint), DrawUtils.getTextCenterY(rect.centerY(), paint), paint);
     }
 
 
-    public boolean drawBackground(Canvas c, Column column, Rect rect,  TableConfig config) {
+    public boolean drawBackground(Canvas c, Column column, Rect rect, TableConfig config) {
         ICellBackgroundFormat<Column> backgroundFormat = config.getColumnCellBackgroundFormat();
-        if(isDrawBg && backgroundFormat != null ){
-            backgroundFormat.drawBackground(c,rect,column,config.getPaint());
+        if (isDrawBg && backgroundFormat != null) {
+            backgroundFormat.drawBackground(c, rect, column, config.getPaint());
             return true;
         }
         return false;
